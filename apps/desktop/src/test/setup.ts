55b2647aded4e0f08112b785fdf370e8dbf8c48d
tangshuang631 <2429936596@qq.com>
2026-06-15 23:13:18 +0800

    test: stabilize app async rendering waits

--- apps/desktop/src/test/setup.ts
+++ apps/desktop/src/test/setup.ts
@@ -1,5 +1,10 @@
 import "@testing-library/jest-dom/vitest";
+import { configure } from "@testing-library/react";
 import { beforeEach } from "vitest";
+
+configure({
+  asyncUtilTimeout: 5000
+});
 
 beforeEach(() => {
   window.localStorage.clear();
