15df214dfb6f9090ade7caa3123347cf7eb320d6
tangshuang631 <2429936596@qq.com>
2026-06-21 20:25:45 +0800

    test(desktop): cleanup mounted app tests

--- apps/desktop/src/test/setup.ts
+++ apps/desktop/src/test/setup.ts
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom/vitest";
-import { configure } from "@testing-library/react";
-import { beforeEach } from "vitest";
+import { cleanup, configure } from "@testing-library/react";
+import { afterEach, beforeEach } from "vitest";
 
 configure({
   asyncUtilTimeout: 5000
@@ -8,4 +8,8 @@ configure({
 
 beforeEach(() => {
   window.localStorage.clear();
+});
+
+afterEach(() => {
+  cleanup();
 });
